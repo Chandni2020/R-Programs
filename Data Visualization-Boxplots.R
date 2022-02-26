1e5319e47b36c124e38d8630780397fc58f9f304
Chandni2020 <chandniparmar025@gmail.com>
2022-02-26 13:02:58 -0500

    Create Data Visualization-Boxplots.R
    boxplots

---
+++ Data Visualization-Boxplots.R
@@ -0,0 +1,12 @@
+library(ggplot2)
+df <- mtcars
+
+pl <- ggplot(df,aes(x=factor(cyl),y=mpg))
+#print(pl+geom_boxplot())
+
+#print(pl+geom_boxplot()+coord_flip())
+#coord_flip is used to flit axis of graph
+
+#print(pl+geom_boxplot(fill='blue'))
+
+print(pl+geom_boxplot(aes(fill=factor(cyl))) + theme_bw())
